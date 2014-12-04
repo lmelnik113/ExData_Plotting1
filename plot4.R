814c3faeaee488527e7e878b3368bfab11b915df
Lisa Melnik <lmelnik113@gmail.com>
2014-12-04 16:09:21 -0500

    version 2

--- plot4.R
+++ plot4.R
@@ -15,14 +15,16 @@ par(mfcol=(c(2,2)))
 #First plot
 plot(datetime,household_data$Global_active_power, 
      type="l", xlab="", 
-     ylab="Global Active Power (kilowatts)")
+     ylab="Global Active Power")
 #Second plot
 plot(datetime,household_data$Sub_metering_1, type="l", 
      xlab="", ylab="Energy sub metering")
 lines(datetime, household_data$Sub_metering_2,col="red")
 lines(datetime, household_data$Sub_metering_3,col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), 
-       lty=c(1,1,1), col=c("black", "red", "blue"))
+       lty=c(1,1,1), bty="n", col=c("black", "red", "blue"))
+#Third plot
 plot(datetime, household_data$Voltage, type="l", ylab="Voltage")
+#Fourth Plot
 plot(datetime, household_data$Global_reactive_power, type="l", ylab="Global_reactive_power")
 dev.off()
